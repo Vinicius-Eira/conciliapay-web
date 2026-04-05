ac4a458a8e19141147f2acf5d0e1957c48139103
Vinícius Eira <viniciusantos.eira@gmail.com>
2026-04-04 21:35:15 -0300

    chore: initial commit with next.js and tailwind

---
+++ src/services/api.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+export const api = axios.create({
+    baseURL: 'http://localhost:8080/api/v1',
+});
+
+api.interceptors.request.use((config) => {
+    const token = Cookies.get('conciliapay.token');
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
